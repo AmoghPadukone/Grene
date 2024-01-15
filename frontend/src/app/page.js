3b89936de872c688fd1e7e8b9df8c9101f0aba82
Amogh <amoghpadukone@gmail.com>
2024-01-15 17:47:43 +0530

    initial commit again

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,7 +1,12 @@
+import HeroSection from "@/components/LandingPage/HeroSection";
 import { Card, Metric, Text } from "@tremor/react";
 import Image from "next/image";
 
 export default function Home() {
   // imprort components here
-  return <div>Grene LAnding Page</div>;
+  return (
+    <main>
+      <HeroSection />
+    </main>
+  );
 }
---
+++ frontend/src/components/LandingPage/HeroSection.js
@@ -0,0 +1,4 @@
+export default function HeroSection() {
+  // imprort components here
+  return <div>Hero component </div>;
+}
