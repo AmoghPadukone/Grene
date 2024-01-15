7de9fd54d0f2428ddc7e45407179de816808beaa
Amogh <amoghpadukone@gmail.com>
2024-01-16 03:06:13 +0530

    Imported the font and got it working

--- frontend/src/components/LandingPage/HeroSection.js
+++ frontend/src/components/LandingPage/HeroSection.js
@@ -1,6 +1,6 @@
 // components/HeroSection.js
 
-import Image from 'next/image';
+import Image from "next/image";
 
 export default function HeroSection() {
   return (
@@ -10,23 +10,21 @@ export default function HeroSection() {
         alt="Hero Logo"
         width={500}
         height={350}
-        className='pl-12'
-        style={{zIndex:"999"}}
+        className="pl-12"
+        style={{ zIndex: "999" }}
       />
       <Image
         src="/heroPattern.svg"
         alt="Hero Pattern"
         width={1200}
         height={800}
-        className='absolute right-0 top-0 z-1'
+        className="absolute right-0 top-0 z-1"
       />
-      <h1 className='text-5xl text-center' style={{ fontFamily:"MuseoModerno,sans-serif"}}>Your one stop for investing <br/>in renewable parks</h1>
-      <Image
-        src="/button.svg"
-        alt="Button"
-        width={300}
-        height={200}
-      />
+      <h1 className="text-5xl text-center font-curvy">
+        Your one stop for investing <br />
+        in renewable parks
+      </h1>
+      <Image src="/button.svg" alt="Button" width={300} height={200} />
     </div>
   );
 }
