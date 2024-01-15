61b41bf44fcc257a773aa605101b4a4e08398eea
Amogh <amoghpadukone@gmail.com>
2024-01-15 17:43:26 +0530

    initial commit

---
+++ frontend/src/app/page.js
@@ -0,0 +1,7 @@
+import { Card, Metric, Text } from "@tremor/react";
+import Image from "next/image";
+
+export default function Home() {
+  // imprort components here
+  return <div>Grene LAnding Page</div>;
+}
