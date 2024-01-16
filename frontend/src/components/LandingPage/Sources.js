ecbe444dada66a15478a36bd77ca448f03b1c5dc
Amogh <amoghpadukone@gmail.com>
2024-01-16 15:56:38 +0530

    steps and sources section, responsive

--- frontend/src/components/LandingPage/Sources.js
+++ frontend/src/components/LandingPage/Sources.js
@@ -1,4 +1,52 @@
+import Image from "next/image";
+import "./LandingPageStyles.css";
+
 export default function Sources() {
   // imprort components here
-  return <div className="h-screen	">Sources section </div>;
+  return (
+    <section className=" overflow-hidden	">
+      <div className="relative ml-[5vw]  py-2 text-5xl lg:text-7xl	font-medium font-curvy">
+        Investment <br />
+        Sources
+      </div>
+      <div className=" mt-[10%] lg:mt-14 xl:mt-0">
+        <div class="orange-wave">
+          <Image
+            draggable="false"
+            src="/solar.svg"
+            alt="solar curve banner"
+            // width={1000}
+            fill={true}
+            // sizes="(max-width: 768px) 100%, (max-width: 1200px) 50vw, 33vw"
+            // height={350}
+            // className="pl-12"
+          />
+        </div>
+        <div class="blue-wave">
+          <Image
+            draggable="false"
+            src="/wind.svg"
+            alt="solar curve banner"
+            // width={1000}
+            fill={true}
+            // sizes="(max-width: 768px) 100%, (max-width: 1200px) 50vw, 33vw"
+            // height={350}
+            // className="pl-12"
+          />
+        </div>
+        <div class="turquoise-wave">
+          <Image
+            draggable="false"
+            src="/hydro.svg"
+            alt="solar curve banner"
+            // width={1000}
+            fill={true}
+            // sizes="(max-width: 768px) 100%, (max-width: 1200px) 50vw, 33vw"
+            // height={350}
+            // className="pl-12"
+          />
+        </div>
+      </div>
+    </section>
+  );
 }
