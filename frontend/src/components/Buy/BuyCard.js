2f72697c04ec2dc94a2a151f30e3e425e4bfa89d
shivam-jainn <shivam.clgstash@gmail.com>
2024-01-17 19:54:53 +0530

    linking done

--- frontend/src/components/Buy/BuyCard.js
+++ frontend/src/components/Buy/BuyCard.js
@@ -1,9 +1,15 @@
+"use client";
 import React from 'react'
 
 import Heading from '../Primitives/Heading'
 import ReserveGrid from '../Reserve/ReserveGrid'
 import { Button } from '../ui/button'
+import { useRouter } from 'next/navigation'
+
 export default function BuyCard() {
+
+    const router = useRouter();
+
     const projectName = "Guj919"
     const company = "Tata solars"
     const source = "Solar"
@@ -17,7 +23,7 @@ export default function BuyCard() {
       <ReserveGrid />
 
       <div className='w-full max-w-md '>
-      <Button className="w-full p-2 my-4">  Reserve {source}</Button>
+      <Button className="w-full p-2 my-4" onClick={()=>{router.push('/checkout')}}>  Reserve {source}</Button>
 
       </div>
       </div>
